bc3fcca09e5894e3e5eac0d74fc193d2a7b765af
mdemong87 <mdemong87@gmail.com>
2025-05-22 05:43:29 +0600

    contact section some accebility update and some design improvement

--- app/componnent/ContactSection.jsx
+++ app/componnent/ContactSection.jsx
@@ -8,15 +8,15 @@ const ContactSection = () => {
             <SectionWraper>
                 <SectionTitle title="Contact" />
                 <div className="grid grid-cols-12 w-full gap-[23px]">
-                    <Input type={"text"} title={"Fast Name*"} />
-                    <Input type={"text"} title={"Fast Name*"} />
-                    <Input type={"text"} title={"Fast Name*"} />
-                    <Input type={"text"} title={"Fast Name*"} />
+                    <Input type={"text"} title={"Fast Name*"} nameId={"fasttName"} />
+                    <Input type={"text"} title={"Last Name*"} nameId={"lastName"} />
+                    <Input type={"email"} title={"Email*"} nameId={"email"} />
+                    <Input type={"phone"} title={"Phone*"} nameId={"phone"} />
                 </div>
                 <div className="w-full flex flex-col pt-[23px]">
-                    <label className="font-medium text-xl pb-[10px] text-[#FFFFFF]" htmlFor='Message'>Message*</label>
-                    <div className="w-full h-fit bgGredient opacity-25 p-[1px] rounded-[10px]">
-                        <textarea name="Message" className="w-full rounded-[10px] h-[124px] p-5 text-gray-200 bg-[#000000]"></textarea>
+                    <label className="font-medium text-lg md:text-xl pb-[10px] text-[#FFFFFF]" htmlFor='Message'>Message*</label>
+                    <div className="w-full h-[124px] inputfiledParant p-[1px] rounded-[10px]">
+                        <textarea name="Message" id="Message" className="w-full rounded-[10px] inputfiled h-[121px] p-5 text-gray-200 bg-[#000000]"></textarea>
                     </div>
                 </div>
 
--- app/componnent/Input.jsx
+++ app/componnent/Input.jsx
@@ -1,9 +1,9 @@
-const Input = ({ type, title }) => {
+const Input = ({ type, title, nameId }) => {
     return (
         <div className="col-span-12 md:col-span-6 w-full flex flex-col">
-            <label className="font-medium text-xl pb-[10px] text-[#FFFFFF]" htmlFor={title}>{title}</label>
-            <div className="w-full borderGradient opacity-25 p-[1px] rounded-[10px]">
-                <input type={type} className="w-full rounded-[10px] h-[48px] p-5 text-gray-200 bg-[#000000] focus:outline-none" name={title} id="title" />
+            <label className="font-medium text-lg md:text-xl pb-[10px] text-[#FFFFFF]" htmlFor={nameId}>{title}</label>
+            <div className="w-full p-[1px] rounded-[10px] inputfiledParant">
+                <input type={type} className="w-full rounded-[10px] h-[48px] p-5 text-gray-200 bg-[#000000] inputfiled" name={nameId} id={nameId} />
             </div>
         </div>
     )
