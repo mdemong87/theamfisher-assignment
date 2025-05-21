6366262fea5d332a71c5b89aa9e0ea9dcb3deac9
mdemong87 <mdemong87@gmail.com>
2025-05-22 05:16:30 +0600

    some Accessibility  bug fix

--- app/componnent/Input.jsx
+++ app/componnent/Input.jsx
@@ -3,7 +3,7 @@ const Input = ({ type, title }) => {
         <div className="col-span-12 md:col-span-6 w-full flex flex-col">
             <label className="font-medium text-xl pb-[10px] text-[#FFFFFF]" htmlFor={title}>{title}</label>
             <div className="w-full borderGradient opacity-25 p-[1px] rounded-[10px]">
-                <input type={type} className="w-full rounded-[10px] h-[48px] p-5 text-gray-200 bg-[#000000] focus:outline-none" name={title} />
+                <input type={type} className="w-full rounded-[10px] h-[48px] p-5 text-gray-200 bg-[#000000] focus:outline-none" name={title} id="title" />
             </div>
         </div>
     )
