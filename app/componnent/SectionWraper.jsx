bc13596da4cbf7d2248ec2b55a13abf1aebe6f3b
mdemong87 <mdemong87@gmail.com>
2025-05-22 04:02:04 +0600

    section wraper fix  align and hero contect position

--- app/componnent/SectionWraper.jsx
+++ app/componnent/SectionWraper.jsx
@@ -1,6 +1,6 @@
 const SectionWraper = ({ children }) => {
     return (
-        <div className="w-full h-full grid grid-cols-12 pt-[120px] pl-[24px] md:pl-[104px] lg:pl-[58px] ">
+        <div className="w-full h-full grid grid-cols-12 pt-[120px] pl-[24px] md:pl-[125px] lg:pl-[58px] ">
             <div className="h-full w-full hidden lg:block col-span-0 lg:col-span-4"></div>
             <div className="col-span-12 lg:col-span-8 h-full w-full pr-[24px] sm:pr[42px] md:pr-[62px] lg:pr-[92px]">
                 {children}
