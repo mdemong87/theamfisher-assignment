bf8f18355a455e572f31b41d11b16583a210b6ed
mdemong87 <mdemong87@gmail.com>
2025-05-22 06:15:28 +0600

    Link or a tag realatated accessibility issues fix

--- app/componnent/SocialIcon.jsx
+++ app/componnent/SocialIcon.jsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 const SocialIcon = ({ link, children }) => {
     return (
-        <a href={link} className="w-[40px] h-[40px] flex justify-center items-center bg-[#121414] text-[#C5C5C5] text-lg rounded-full transition-all duration-300 hover:bg-linear-to-r from-[#F5BD4D] to-[#F89222] hover:text-white">
+        <Link href={link} className="w-[40px] h-[40px] flex justify-center items-center bg-[#121414] text-[#C5C5C5] text-lg rounded-full transition-all duration-300 hover:bg-linear-to-r from-[#F5BD4D] to-[#F89222] hover:text-white">
             {children}
-        </a>
+        </Link>
     )
 }
 
