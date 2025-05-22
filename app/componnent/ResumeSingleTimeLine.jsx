41ab97eb1b09390c8fedd113e9c28206ef23aa80
mdemong87 <mdemong87@gmail.com>
2025-05-23 00:14:20 +0600

    resume section small screen alignment issues fixed

--- app/componnent/ResumeSingleTimeLine.jsx
+++ app/componnent/ResumeSingleTimeLine.jsx
@@ -4,12 +4,12 @@ import Timeline from "./Timeline";
 const ResumeSingleTimeLine = ({ timeline, title, subtitle }) => {
     return (
         <div className="mt-5 -translate-x-[12px] flex flex-col gap-[12px]">
-            <div className="flex gap-9 items-center">
+            <div className="flex gap-2 sm:gap-4 md:gap-9 items-center">
                 <div className="w-[16px] h-[16px] rotate-45 border-[2px] border-[#646464] bg-[#121414]" />
                 <Timeline timeline={timeline} />
 
             </div>
-            <div className="flex gap-9 items-center">
+            <div className="flex gap-2 sm:gap-4 md:gap-9 items-center">
                 <div className="w-[16px] h-[16px] invisible" />
                 <ResumeDetailsBox title={title} subtitle={subtitle} />
             </div>
